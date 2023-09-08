7c94ca6cd1e39b6a1afda65c9400b7d576864f8c
davis <davisfpatterson@gmail.com>
2023-09-08 11:14:51 -0400

    got autoPlay working on the film slides, sort function not working

--- react-slideshow/src/App.jsx
+++ react-slideshow/src/App.jsx
@@ -1,49 +1,110 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import FilmCard from './FilmCard';
 import filmData from './film-data.json';
-import ghibliCatImg from './assets/ghibli-cat.png'
-import ghibliKaonashiImg from './assets/ghibli-kaonashi.png'
+import ghibliCatImg from './assets/ghibli-cat.png';
+import ghibliKaonashiImg from './assets/ghibli-kaonashi.png';
 
 function App() {
   const [films, setFilms] = useState(filmData);
-  const initialValue = 0
-  const [activeFilmIndex, setActiveFilmIndex] = useState(initialValue);
-  const lastFilmIndex = filmData.length - 1
+  const initialIndexValue = 0;
+  const [activeFilmIndex, setActiveFilmIndex] = useState(initialIndexValue);
+  const lastFilmIndex = filmData.length - 1;
+  const sortedFilms = [...filmData];
+  const [timer, setTimer] = useState(null);
 
+  // const sortByReleaseDate = (filmData) => {
+  //   // need to use the .sort js function to sort by release_date
+  //   // => assign to variable, sorted_data = result
+  //   // update state to use new sorted list instead of filmData
 
-  const sortByReleaseDate = (filmData) => {
-    // need to use the .sort js function to sort by release_date 
-    // => assign to variable, sorted_data = result
-    // update state to use new sorted list instead of filmData
-  }
+  //   sortedFilms.sort((a, b) => {
+  //     const releaseDateA = parseInt(a.release_date);
+  //     const releaseDateB = parseInt(b.release_date);
+  //     return releaseDateA - releaseDateB;
+  //   });
+  //   setFilms(sortedFilms);
+  // };
 
-    return (
-      <>
-        <header>
-          <div className="ghibliContainer">
-            <div className="ghibliBox">
-              <h1 className="ghibliText">Studio Ghibli</h1>
-              <p className="ghibliJapanese">株式会社スタジオジブリ</p>
-            </div>
+  const autoProgNextFilm = () => {
+    if (activeFilmIndex < lastFilmIndex) {
+      setActiveFilmIndex((index) => index + 1);
+    } else {
+      setActiveFilmIndex(initialIndexValue);
+    }
+  }; // Make sure this function is properly closed with a curly brace
+
+  useEffect(() => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    const newTimer = setTimeout(() => {
+      autoProgNextFilm();
+    }, 4000);
+
+    setTimer(newTimer);
+
+    return () => {
+      clearTimeout(newTimer);
+    };
+  }, [activeFilmIndex]);
+
+  return (
+    <>
+      <header>
+        <div className='ghibliContainer'>
+          <div className='ghibliBox'>
+            <h1 className='ghibliText'>Studio Ghibli</h1>
+            <p className='ghibliJapanese'>株式会社スタジオジブリ</p>
           </div>
-          <img className="ghibliCat" src={ghibliCatImg} alt="ghibli-cat"></img>
-          <img className="ghibliKaonashi" src={ghibliKaonashiImg} alt="ghibli-kaonashi"></img>
-        </header>
-        {films.map((film, index) => (
-          activeFilmIndex === index &&
+        </div>
+        <img className='ghibliCat' src={ghibliCatImg} alt='ghibli-cat'></img>
+        <img
+          className='ghibliKaonashi'
+          src={ghibliKaonashiImg}
+          alt='ghibli-kaonashi'
+        ></img>
+      </header>
+      {films.map(
+        (film, index) =>
+          activeFilmIndex === index && (
             <div key={index}>
-              <FilmCard film={film}/>
+              <FilmCard film={film} />
             </div>
           )
-        )}
-        <div className='buttonBox'>
-          <button className="refreshButton" onClick={() => setActiveFilmIndex(initialValue)} disabled={activeFilmIndex === initialValue}>⟳ Refresh</button>
-          <button className="backButton" onClick={() => setActiveFilmIndex(activeFilmIndex - 1)} disabled={activeFilmIndex === initialValue}>&#60; Back</button>
-          <button className="nextButton" onClick={() => setActiveFilmIndex(activeFilmIndex + 1)} disabled={activeFilmIndex === lastFilmIndex}>Next &#62;</button>
-        </div>
-      </>
-    );
-  }
+      )}
+      <div className='buttonBox'>
+        <button
+          className='sortButton'
+          onClick={() => setFilms(sortByReleaseDate)}
+        >
+          sort
+        </button>
+        <button
+          className='refreshButton'
+          onClick={() => setActiveFilmIndex(initialIndexValue)}
+          disabled={activeFilmIndex === initialIndexValue}
+        >
+          ⟳ Refresh
+        </button>
+        <button
+          className='backButton'
+          onClick={() => setActiveFilmIndex(activeFilmIndex - 1)}
+          disabled={activeFilmIndex === initialIndexValue}
+        >
+          &#60; Back
+        </button>
+        <button
+          className='nextButton'
+          onClick={() => setActiveFilmIndex(activeFilmIndex + 1)}
+          disabled={activeFilmIndex === lastFilmIndex}
+        >
+          Next &#62;
+        </button>
+      </div>
+    </>
+  );
+}
 
-export default App
+export default App;
